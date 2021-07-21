949ce578693f2a024df02a271c8df503b14fa5a9
hagai <hagai.e@visiblerisk.com>
2021-07-21 15:20:48 +0300

    added config

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -81,6 +81,8 @@ THIRD_PARTY_APPS = [
     "dj_rest_auth",
     "django_filters",
     "taggit",
+    'location_field',
+    'dlf_app',
 ]
 
 LOCAL_APPS = [
@@ -333,3 +335,35 @@ JAZZMIN_SETTINGS = {
 }
 
 TAGGIT_CASE_INSENSITIVE = True
+
+LOCATION_FIELD_PATH = STATIC_URL + 'location_field'
+
+LOCATION_FIELD = {
+    'map.provider': 'google',
+    'map.zoom': 13,
+
+    'search.provider': 'google',
+    'search.suffix': '',
+
+    # Google
+    'provider.google.api': '//maps.google.com/maps/api/js?sensor=false',
+    'provider.google.api_key': '<INSERT GOOGLE API KEY>',
+    'provider.google.api_libraries': '',
+    'provider.google.map.type': 'ROADMAP',
+
+    # Mapbox
+    'provider.mapbox.access_token': '',
+    'provider.mapbox.max_zoom': 18,
+    'provider.mapbox.id': 'mapbox.streets',
+
+    # OpenStreetMap
+    'provider.openstreetmap.max_zoom': 18,
+
+    # misc
+    'resources.root_path': LOCATION_FIELD_PATH,
+    'resources.media': {
+        'js': (
+            LOCATION_FIELD_PATH + '/js/form.js',
+        ),
+    },
+}
