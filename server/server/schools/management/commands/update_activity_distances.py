2984b58375fb04184ad6a175a7e4b588295f36fc
yael-team88 <yael.s@visiblerisk.com>
2021-07-22 11:59:18 +0000

    add distance scripts

--- server/server/schools/management/commands/update_activity_distances.py
+++ server/server/schools/management/commands/update_activity_distances.py
@@ -1,15 +1,85 @@
+import googlemaps
+import haversine as hs
 from django.core.management.base import BaseCommand
+from haversine import Unit
+
+from server.organizations.models import Activity
+from server.schools.models import School
 
 
 class Command(BaseCommand):
+    API_KEY = "AIzaSyDKkYpjJONrNZN-BtOXR7ZhSEqSI4pnSdo"
+    ACTIVITY_DICT = {
+        "5_and_less": {},
+        "10_and_less": {},
+        "15_and_less": {},
+        "20_and_less": {},
+        "25_and_less": {},
+        "30_and_less": {},
+    }
     help = "Updates the activity distance cache for schools"
 
     def add_arguments(self, parser):
         "parser.add_argument('some_number', nargs='+', type=int)"
         pass
 
-    def create_all(self, entitiesPrefix=""):
-        pass
+    def __check_google_distance(self, loc1, loc2):
+        gmaps = googlemaps.Client(key=self.API_KEY)
+        result = gmaps.distance_matrix(loc1, loc2, mode="driving")
+        try:
+            return True, result["rows"][0]["elements"][0]["duration"]["value"]
+        except KeyError:
+            return False, result["rows"][0]["elements"][0]["duration"]["value"]
+
+    def __check_linear_distance(self, school_coords, org_location):
+        return hs.haversine(school_coords, org_location, unit=Unit.KILOMETERS)
+
+    def create_all(self, school):
+        for activity in Activity.objects.all():
+            org_location = tuple(float(coord) for coord in activity.location.split(","))
+            school_coords = tuple(float(coord) for coord in school.location.split(","))
+            free_distance = self.__check_linear_distance(school_coords, org_location)
+            if free_distance < 5:
+                google_status = self.__check_google_distance(
+                    school_coords, org_location
+                )
+                self.ACTIVITY_DICT["5_and_less"][activity.slug] = {
+                    "google_distance": google_status[0],
+                    "distance": google_status[1],
+                }
+            elif free_distance < 10:
+                self.ACTIVITY_DICT["10_and_less"][activity.slug] = {
+                    "google_distance": False,
+                    "distance": free_distance,
+                }
+            elif free_distance < 15:
+                self.ACTIVITY_DICT["15_and_less"][activity.slug] = {
+                    "google_distance": False,
+                    "distance": free_distance,
+                }
+            elif free_distance < 20:
+                self.ACTIVITY_DICT["20_and_less"][activity.slug] = {
+                    "google_distance": False,
+                    "distance": free_distance,
+                }
+            elif free_distance < 25:
+                self.ACTIVITY_DICT["25_and_less"][activity.slug] = {
+                    "google_distance": False,
+                    "distance": free_distance,
+                }
+            elif free_distance < 30:
+                self.ACTIVITY_DICT["30_and_less"][activity.slug] = {
+                    "google_distance": False,
+                    "distance": free_distance,
+                }
+
+        return self.ACTIVITY_DICT
 
     def handle(self, *args, **options):
-        self.create_all()
+        for school in School.objects.all():
+            distance_cache = self.create_all(school)
+            import pdb
+
+            pdb.set_trace()
+            school.activity_distance_cache = distance_cache
+            school.save()
--- server/server/users/management/commands/constants.py
+++ server/server/users/management/commands/constants.py
@@ -10,8 +10,7 @@ ORGANIZATION_PAYLOAD = {
     "number_of_employees": 100,
     "number_of_members": 200,
     "number_of_volunteers": 30,
-    "location_lon": 32.109333,
-    "location_lat": 34.855499,
+    "location": "32.0732734,34.7970061",
     "address_city": "city",
     "address_street": "addr",
     "address_house_num": "13",
@@ -31,6 +30,7 @@ SCHOOL_PAYLOAD = {
     "contact_phone": "0521234567",
     "website": "https://school.com",
     "grade_levels": [1, 2, 3, 4, 5, 6],
+    "location": "32.075447434961156,34.79573965072632",
 }
 
 ACTIVITY_PAYLOADS = [
@@ -42,6 +42,7 @@ ACTIVITY_PAYLOADS = [
         "contact_name": "John Smith",
         "logo": "https://picsum.photos/200/300",
         "phone_number": "0521234567",
+        "location": "40.076792904088855,76.79646921157836",
     },
     {
         "name": "Activity Two",
@@ -51,6 +52,7 @@ ACTIVITY_PAYLOADS = [
         "contact_name": "David Cohen",
         "logo": "https://picsum.photos/200/300",
         "phone_number": "0521234567",
+        "location": "32.076792904088855,34.79646921157836",
     },
 ]
 
